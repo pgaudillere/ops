f08044fd0a3e0cd296405000a25306e767faaa9f
anton.gravestam <anton.gravestam@bfec6ad8-f234-11dd-ac65-f74f249cfaac>
2010-07-02 13:53:54 +0000

    Added base for CodeGenerator plugins via netbeans module system

--- trunk/Tools/NBOPSIDLSupport/src/ops/netbeansmodules/idlsupport/ProjectIDLCompiler.java
+++ trunk/Tools/NBOPSIDLSupport/src/ops/netbeansmodules/idlsupport/ProjectIDLCompiler.java
@@ -8,15 +8,19 @@ import configlib.exception.FormatException;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Vector;
 import ops.OPSConfig;
 import ops.netbeansmodules.idlsupport.compilers.CppCompiler;
 import ops.netbeansmodules.idlsupport.compilers.DebugProjectCompiler;
 import ops.netbeansmodules.idlsupport.compilers.JavaCompiler;
 import ops.netbeansmodules.idlsupport.compilers.JavaOPSConfigCompiler;
+import ops.netbeansmodules.idlsupport.godegenerator.OPSConfigCompiler;
 import ops.netbeansmodules.idlsupport.compilers.VisualStudio2008CppExampleCompiler;
+import ops.netbeansmodules.idlsupport.godegenerator.OPSCodeGenerator;
 import org.openide.filesystems.FileObject;
 import org.openide.util.Exceptions;
+import org.openide.util.Lookup;
 import parsing.IDLClass;
 
 /**
@@ -31,7 +35,6 @@ public class ProjectIDLCompiler
     CppCompiler cppCompiler = new CppCompiler();
     DebugProjectCompiler debugProjectCompiler = new DebugProjectCompiler();
     VisualStudio2008CppExampleCompiler cppExampleCompiler = new VisualStudio2008CppExampleCompiler();
-
     OPSIDLProject project;
 
     public ProjectIDLCompiler(OPSIDLProject project)
@@ -43,19 +46,17 @@ public class ProjectIDLCompiler
     {
         for (FileObject childFileObject : fileObject.getChildren())
         {
-            if(!childFileObject.getName().equals(".svn") && !childFileObject.getName().equals(".cvs"))
+            if (!childFileObject.getName().equals(".svn") && !childFileObject.getName().equals(".cvs"))
             {
-                if(childFileObject.isFolder())
+                if (childFileObject.isFolder())
                 {
                     recursiveDelete(childFileObject);
-                }
-                else
+                } else
                 {
                     try
                     {
                         childFileObject.delete();
-                    }
-                    catch (IOException ex)
+                    } catch (IOException ex)
                     {
                         Exceptions.printStackTrace(ex);
                     }
@@ -67,16 +68,27 @@ public class ProjectIDLCompiler
 
     public void compile(Vector<IDLClass> idlClasses)
     {
+        //Test plugins
+
+
+
         FileObject projectDirectory = project.getProjectDirectory();
         for (FileObject fileObject : projectDirectory.getChildren())
         {
-            if(fileObject.getName().equals("Generated"))
+            if (fileObject.getName().equals("Generated"))
             {
                 recursiveDelete(fileObject);
             }
         }
-        
-        
+        Collection<? extends OPSCodeGenerator> codeGenerators = Lookup.getDefault().lookupAll(OPSCodeGenerator.class);
+        for (OPSCodeGenerator codeGenerator : codeGenerators)
+        {
+            codeGenerator.compileDataClasses(idlClasses, project.getProjectDirectory().getPath() + "/Generated/");
+            codeGenerator.compileConfig(null, project.getProjectDirectory().getPath() + "/Generated/");
+            codeGenerator.build(project.getProjectDirectory().getPath() + "/Generated/");
+        }
+
+
         if (project.getProperties().generateCpp)
         {
             cppCompiler.compileDataClasses(idlClasses, project.getProjectDirectory().getPath() + "/Generated/");
@@ -89,9 +101,18 @@ public class ProjectIDLCompiler
             try
             {
                 opsConfig = OPSConfig.getConfig(new File(project.getProjectDirectory().getPath() + "/" + project.getProperties().defaultOPSTopicConfigFile));
-                ArrayList<String> generatedFiles = javaOPSConfigCompiler.compileConfig(opsConfig, project.getProjectDirectory().getPath() + "/Generated/");
+//                ArrayList<String> generatedFiles = javaOPSConfigCompiler.compileConfig(opsConfig, project.getProjectDirectory().getPath() + "/Generated/");
+//
+//                javaCompiler.appendFileToBuild(generatedFiles);
 
-                javaCompiler.appendFileToBuild(generatedFiles);
+                Collection<? extends OPSConfigCompiler> configCompilers = Lookup.getDefault().lookupAll(OPSConfigCompiler.class);
+                for (OPSConfigCompiler configCompiler : configCompilers)
+                {
+                    ArrayList<String> generatedFiles = configCompiler.compileConfig(opsConfig, project.getProjectDirectory().getPath() + "/Generated/");
+                    javaCompiler.appendFileToBuild(generatedFiles);
+                }
+
+
 
             } catch (IOException ex)
             {
@@ -117,17 +138,17 @@ public class ProjectIDLCompiler
                     Exceptions.printStackTrace(ex);
                 }
             }
-            
-            
+
+
 
 
         }
-        if(project.getProperties().buildDebugProject)
+        if (project.getProperties().buildDebugProject)
         {
             debugProjectCompiler.createDebugProjectFile(project.getProjectDirectory().getPath(), project.getProjectDirectory().getName(), project.getProperties());
 
         }
-        if(Boolean.parseBoolean(project.getProperties().getPropertyValue("vsExampleEnabled", "false")))
+        if (Boolean.parseBoolean(project.getProperties().getPropertyValue("vsExampleEnabled", "false")))
         {
             cppExampleCompiler.compileVSCppExample(project.getProjectDirectory().getPath(), project.getProjectDirectory().getName(), project.getProperties());
         }
