afff77558479e570ae7d60a65c41608f4a3d3950
LelleAnd@gmail.com <LelleAnd@gmail.com@bfec6ad8-f234-11dd-ac65-f74f249cfaac>
2012-11-06 21:23:47 +0000

    C++: Refactoring of SendDataHandlers

--- trunk/Cpp/include/SendDataHandler.h
+++ trunk/Cpp/include/SendDataHandler.h
@@ -21,17 +21,51 @@
 #define	ops_SendDataHandler_h
 
 #include "Topic.h"
+#include "Sender.h"
 
 namespace ops
 {
-	//Interface
 	class SendDataHandler
 	{
 	public:
 		virtual bool sendData(char* buf, int bufSize, Topic& topic) = 0;
 
-		virtual void addPublisher(void* client) = 0;
-		virtual void removePublisher(void* client) = 0;
+		virtual void addPublisher(void* client) 
+		{
+            SafeLock lock(&mutex);
+			// Check that it isn't already in the list
+			for (unsigned int i = 0; i < publishers.size(); i++) {
+				if (publishers[i] == client) return;
+			}
+			// Save client in the list
+			publishers.push_back(client);
+			// For the first client, we open the sender
+			if (publishers.size() == 1) sender->open();
+		}
+
+		virtual void removePublisher(void* client)
+		{
+            SafeLock lock(&mutex);
+			// Remove it from the list
+			std::vector<void*>::iterator Iter;
+			for (Iter = publishers.begin(); Iter != publishers.end(); Iter++) {
+				if (*Iter == client) {
+					publishers.erase(Iter);
+					break;
+				}
+			}
+			if (publishers.size() == 0) sender->close();
+		}
+
+	protected:
+
+        Sender* sender;
+        Lockable mutex;
+
+	private:
+
+		std::vector<void*> publishers;
+
 	};
 }
 
